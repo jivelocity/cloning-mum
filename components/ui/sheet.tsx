4675fad10716cb8173c67ebe8ec938b05b5ce5fd
unknown <rajibaqil11@gmail.com>
2023-07-17 07:14:49 +0700

    delete image and footer

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -6,7 +6,6 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import Button from "./Button"
 
 const Sheet = SheetPrimitive.Root
 
